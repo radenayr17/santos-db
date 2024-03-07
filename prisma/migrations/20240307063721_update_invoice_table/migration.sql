3bf5b5946108ac532e24c8583b973ef2d5ef1b9a
radenayr <radenayr@outlook.com>
2024-03-07 14:37:40 +0800

    add invoice type

--- prisma/migrations/20240307063721_update_invoice_table/migration.sql
+++ prisma/migrations/20240307063721_update_invoice_table/migration.sql
@@ -1,5 +1,6 @@
 -- AlterTable
-ALTER TABLE `Invoice` ADD COLUMN `updatedByUuid` VARCHAR(191) NULL;
+ALTER TABLE `Invoice` ADD COLUMN `type` VARCHAR(191) NOT NULL DEFAULT 'invoice',
+    ADD COLUMN `updatedByUuid` VARCHAR(191) NULL;
 
 -- AddForeignKey
 ALTER TABLE `Invoice` ADD CONSTRAINT `Invoice_updatedByUuid_fkey` FOREIGN KEY (`updatedByUuid`) REFERENCES `User`(`uuid`) ON DELETE SET NULL ON UPDATE CASCADE;
